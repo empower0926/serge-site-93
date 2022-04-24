9cb0e414bb5a818630840e11d01cc50cb53aa9c6
Rsmile310 <78564494+Rsmile310@users.noreply.github.com>
2022-04-24 21:47:13 +0900

    Initial commit

---
+++ src/layouts/dashboard/header/index.js
@@ -0,0 +1,89 @@
+import PropTypes from 'prop-types';
+// @mui
+import { styled } from '@mui/material/styles';
+import { Box, Stack, AppBar, Toolbar } from '@mui/material';
+// hooks
+import useOffSetTop from '../../../hooks/useOffSetTop';
+import useResponsive from '../../../hooks/useResponsive';
+// utils
+import cssStyles from '../../../utils/cssStyles';
+// config
+import { HEADER, NAVBAR } from '../../../config';
+// components
+import Logo from '../../../components/Logo';
+import Iconify from '../../../components/Iconify';
+import { IconButtonAnimate } from '../../../components/animate';
+//
+import LanguagePopover from './LanguagePopover';
+
+// ----------------------------------------------------------------------
+
+const RootStyle = styled(AppBar, {
+  shouldForwardProp: (prop) => prop !== 'isCollapse' && prop !== 'isOffset' && prop !== 'verticalLayout',
+})(({ isCollapse, isOffset, verticalLayout, theme }) => ({
+  ...cssStyles(theme).bgBlur(),
+  boxShadow: 'none',
+  background: '#000',
+  height: HEADER.MOBILE_HEIGHT,
+  zIndex: theme.zIndex.appBar + 1,
+  transition: theme.transitions.create(['width', 'height'], {
+    duration: theme.transitions.duration.shorter,
+  }),
+  [theme.breakpoints.up('lg')]: {
+    height: HEADER.DASHBOARD_DESKTOP_HEIGHT,
+    width: '100%',
+    ...(isCollapse && {
+      width: `calc(100% - ${NAVBAR.DASHBOARD_COLLAPSE_WIDTH}px)`,
+    }),
+    ...(isOffset && {
+      height: HEADER.DASHBOARD_DESKTOP_OFFSET_HEIGHT,
+    }),
+    ...(verticalLayout && {
+      width: '100%',
+      height: HEADER.DASHBOARD_DESKTOP_OFFSET_HEIGHT,
+      backgroundColor: theme.palette.background.default,
+    }),
+  },
+}));
+
+// ----------------------------------------------------------------------
+
+DashboardHeader.propTypes = {
+  onOpenSidebar: PropTypes.func,
+  isCollapse: PropTypes.bool,
+  verticalLayout: PropTypes.bool,
+};
+
+export default function DashboardHeader({ onOpenSidebar, isCollapse = false, verticalLayout = false }) {
+  const isOffset = useOffSetTop(HEADER.DASHBOARD_DESKTOP_HEIGHT) && !verticalLayout;
+
+  const isDesktop = useResponsive('up', 'lg');
+
+  return (
+    <RootStyle isCollapse={isCollapse} isOffset={isOffset} verticalLayout={verticalLayout}>
+      <Toolbar
+        sx={{
+          minHeight: '100% !important',
+          px: { lg: 5 },
+          display: 'flex',
+          justifyContent: 'space-between'
+        }}
+      >
+        {isDesktop && verticalLayout && <Logo sx={{ mr: 2.5 }} />}
+        <Box>
+          <IconButtonAnimate onClick={onOpenSidebar} sx={{ mr: 1, color: '#fff', fontSize: '36px' }}>
+            <Iconify icon="eva:menu-2-fill" />
+          </IconButtonAnimate>
+        </Box>
+        <Box>
+          <IconButtonAnimate>
+            <Logo sx={{ mr: 2.5 }} />
+          </IconButtonAnimate>
+        </Box>
+        <Stack direction="row" alignItems="center" spacing={{ xs: 0.5, sm: 1.5 }}>
+          <LanguagePopover />
+        </Stack>
+      </Toolbar>
+    </RootStyle>
+  );
+}
