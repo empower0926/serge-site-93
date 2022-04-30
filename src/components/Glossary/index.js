bf35d303657b3cfde798bf96a73a7ed9d7b86ccb
Rsmile310 <78564494+Rsmile310@users.noreply.github.com>
2022-04-30 21:24:10 +0900

    update

--- src/components/Glossary/index.js
+++ src/components/Glossary/index.js
@@ -18,40 +18,46 @@ export default function Glossary() {
 
   return (
     <Container sx={{ maxWidth: '1200px', py: 6 }}>
-      <Stack direction="row" mb={4} sx={{ flexWrap: 'wrap' }}>
-        <Box
-          variant="span"
-          onClick={() => {
-            handleSearch('All');
-          }}
-        >
-          <KeyButton title="All" variant="text" />
-        </Box>
-        {GlossaryConfig.map((e, index) => (
+      <MotionViewport>
+        <Stack direction="row" mb={4} sx={{ flexWrap: 'wrap' }}>
           <Box
-            key={index}
             variant="span"
             onClick={() => {
-              handleSearch(e.title);
+              handleSearch('All');
             }}
           >
-            <KeyButton title={translate(e.title)} variant="text" />
+            <KeyButton title="All" variant="text" />
           </Box>
-        ))}
-      </Stack>
-      <Stack>
-        {GlossaryConfig.map((e, index) => (
-          <Stack key={index} mb={3} sx={{ display: `${keyWord === 'All' || keyWord === e.title ? 'block' : 'none'}` }}>
-            <Typography variant="h5" fontWeight="600">
-              {translate(e.title)}
-            </Typography>
-            <Divider sx={{ my: 1 }} />
-            <Typography variant="body1" fontSize="18px">
-              {translate(e.content)}
-            </Typography>
-          </Stack>
-        ))}
-      </Stack>
+          {GlossaryConfig.map((e, index) => (
+            <Box
+              key={index}
+              variant="span"
+              onClick={() => {
+                handleSearch(e.title);
+              }}
+            >
+              <KeyButton title={translate(e.title)} variant="text" />
+            </Box>
+          ))}
+        </Stack>
+        <Stack>
+          {GlossaryConfig.map((e, index) => (
+            <Stack
+              key={index}
+              mb={3}
+              sx={{ display: `${keyWord === 'All' || keyWord === e.title ? 'block' : 'none'}` }}
+            >
+              <Typography variant="h5" fontWeight="600">
+                {translate(e.title)}
+              </Typography>
+              <Divider sx={{ my: 1 }} />
+              <Typography variant="body1" fontSize="18px">
+                {translate(e.content)}
+              </Typography>
+            </Stack>
+          ))}
+        </Stack>
+      </MotionViewport>
     </Container>
   );
 }
