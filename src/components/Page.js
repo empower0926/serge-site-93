3d23f01c15ba913035e95cf68e3ebb10bbf1acdf
Ameer Hamza <hamzagee@outlook.com>
2022-05-24 12:15:16 +0500

    greesensitive spell change

--- src/components/Page.js
+++ src/components/Page.js
@@ -9,7 +9,7 @@ import { Box } from '@mui/material';
 const Page = forwardRef(({ children, title = '', meta, ...other }, ref) => (
   <>
     <Helmet>
-      <title>{`${title} | Greansensitive`}</title>
+      <title>{`${title} | Greesensitive`}</title>
       {meta}
     </Helmet>
 
